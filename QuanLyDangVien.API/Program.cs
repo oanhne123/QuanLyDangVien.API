fdf9d960de7a9cca57dfc695f4c5311d576a9cbd
oanhne123 <caoduyoanh123@gmail.com>
2026-05-07 14:51:42 +0700

    Update Program.cs

--- QuanLyDangVien.API/Program.cs
+++ QuanLyDangVien.API/Program.cs
@@ -45,11 +45,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 //app.UseHttpsRedirection();
 // PHẢI NẰM Ở ĐÂY: Sau UseRouting và Trước UseAuthorization
